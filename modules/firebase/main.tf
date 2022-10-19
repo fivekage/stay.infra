5a3dcec0f628dafb4bdb2c8ca38961bb2b998362
Samy Mokhtari <samy.mokhtari213@gmail.com>
2022-10-19 14:35:45 +0200

    firebase web app

--- modules/firebase/main.tf
+++ modules/firebase/main.tf
@@ -1,5 +1,40 @@
 # DATABASE
-resource "google_firebase_project" "firebase_database" {
+resource "google_firebase_project" "this" {
   provider = google-beta
   project  = var.project_id
+}
+
+resource "google_firebase_web_app" "basic" {
+  provider     = google-beta
+  project      = google_project.this.project_id
+  display_name = "Display Name Basic"
+
+  depends_on = [google_firebase_project.this]
+}
+
+data "google_firebase_web_app_config" "basic" {
+  provider   = google-beta
+  web_app_id = google_firebase_web_app.basic.app_id
+}
+
+resource "google_storage_bucket" "this" {
+  provider = google-beta
+  name     = "fb-webapp-"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "this" {
+  provider = google-beta
+  bucket   = google_storage_bucket.this.name
+  name     = "firebase-config.json"
+
+  content = jsonencode({
+    appId             = google_firebase_web_app.basic.app_id
+    apiKey            = data.google_firebase_web_app_config.basic.api_key
+    authDomain        = data.google_firebase_web_app_config.basic.auth_domain
+    databaseURL       = lookup(data.google_firebase_web_app_config.basic, "database_url", "")
+    storageBucket     = lookup(data.google_firebase_web_app_config.basic, "storage_bucket", "")
+    messagingSenderId = lookup(data.google_firebase_web_app_config.basic, "messaging_sender_id", "")
+    measurementId     = lookup(data.google_firebase_web_app_config.basic, "measurement_id", "")
+  })
 }
