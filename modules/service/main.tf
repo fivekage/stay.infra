811023be362d86f8eaa740f7fbf55f1fb33b258f
Samy Mokhtari <samy.mokhtari213@gmail.com>
2022-10-22 19:36:04 +0200

    fix services

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -3,4 +3,18 @@ resource "google_project_service" "this" {
   service = var.service_id
 
   disable_dependent_services = true
+  disable_on_destroy         = false
+  provisioner "local-exec" {
+    command = <<EOF
+for i in {1..5}; do
+  sleep $i
+  if gcloud services list --project="${var.project_id}" | grep "${var.service_id}"; then
+    exit 0
+  fi
+done
+
+echo "Service was not enabled after 15s"
+exit 1
+EOF
+  }
 }
